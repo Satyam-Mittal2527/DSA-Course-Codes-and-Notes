46b91faa3fa18e245e9d255b2ac9e733a466e04b
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2026-05-16 03:55:48 +0530

    first commit

--- SortingAlgo.java
+++ SortingAlgo.java
@@ -33,6 +33,17 @@ class SelectionSort{
         
         return arr;
     }
+    public static int[] InsertionSort(int[] arr, int n){
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j>0 && arr[j-1]> arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+        return arr;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
@@ -43,6 +54,7 @@ class SelectionSort{
         int n = arr.length;
         // arr = SelectionSort(arr,n);
         arr = BubbleSort(arr,n);
+        arr = InsertionSort(arr,n);
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
