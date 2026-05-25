ef9f362da56ef6933701c725330d8f257c924e48
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2026-05-25 14:54:58 +0530

    ArrayRotate

---
+++ SortingAlgo/Array/RotateByKplace.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class RotateByKplace{
+    public static void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        k = k%n;
+        reverse(nums,0,n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+
+        for(int i=0;i<n;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
