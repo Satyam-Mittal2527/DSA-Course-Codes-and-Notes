e4ca6ad38b2effa1bda22d72f3efac00f600a165
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2026-06-01 13:45:06 +0530

    DPinStocks

---
+++ DynamicProgramming/DPinStocks.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int min = prices[0];
+       int profit = 0;
+       int maxProfit = 0;
+       int n = prices.length;
+       for(int i=0; i< n;i++){
+        profit = prices[i] - min;
+        maxProfit = Math.max(maxProfit, profit);
+        min = Math.min(min, prices[i]);
+       }
+
+       return maxProfit;
+    }
+}
